07915f8ed87c0981c99b6b644a85309bfec2a95b
baolong24 <1667375308@qq.com>
2021-05-06 22:13:56 +0800

    gauguin: Adopt it for gauguin

---
+++ gauguin-sepolicy.mk
@@ -0,0 +1,42 @@
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/xiaomi/gauguin-sepolicy/private
+
+# NXP Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/gauguin-sepolicy/vendor/nxp
+
+# Lineage Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/gauguin-sepolicy/vendor/lineage
+
+# Qcom Sepolicy
+BOARD_SEPOLICY_DIRS += \
+	device/xiaomi/gauguin-sepolicy/vendor/qcom
+
+# Xiaomi Sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/adbd \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/audio \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/battery \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/block \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/camera \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/citsensor \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/displayfeature \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/fingerprint \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/fod \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/graphics \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/health \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/ir \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/last_kmsg \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/light \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/motor \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/mlipay \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/parts \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/power \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/radio \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/sensor \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/thermald \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/touchfeature \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/usb \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/wireless \
+    device/xiaomi/gauguin-sepolicy/vendor/xiaomi/wlan
